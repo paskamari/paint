7ef794b1939e9d6cefe894645299ad9feafa8db7
Hamid Paskamari <hpaskamari@gmail.com>
2013-05-21 23:41:28 +0430

    update canvas.js

--- script/canvas.js
+++ script/canvas.js
@@ -1,29 +1,51 @@
-var
-	cnv = document.getElementById('paint'),
-	ctx = cnv.getContext('2d'),
-	painting = false;
+(function(){
 
-ctx.strokeStyle = 'black';
+	var cnv = document.getElementById('paint');
+	var ctx = cnv.getContext('2d');
+	var painting = false;
+	var a= 2;
 
-cnv.onmousemove = function (event){
-	if(painting){
+	function colorB(color){
+		b = color;
+		return b;
+	}
+
+	var sizebox = document.querySelectorAll('.sizebox li');
+
+	for(var i=0;i<sizebox.length;i++){
+		sizebox.item(i).style.width = (i+8)+'px';
+		sizebox.item(i).style.height = (i+8)+'px';
+	}
+
+	for(var i=0;i<sizebox.length;i++){
+		(function(j){
+			sizebox.item(i).onclick = function(){
+				a = (2*j)+1;
+			}				
+		})(i);
+	}
+
+	cnv.onmousemove = function (event){
+		if(painting){
+			var x = event.offsetX;
+			var y = event.offsetY;
+			ctx.lineTo(x,y);
+			ctx.stroke();
+		}
+	};
+
+	cnv.onmousedown=function(event){
 		var x = event.offsetX;
 		var y = event.offsetY;
-		ctx.lineTo(x,y);
-		ctx.stroke();
+		ctx.beginPath();
+		ctx.moveTo(x,y);
+		ctx.strokeStyle=b;
+		ctx.lineWidth=a;
+		painting = true;
 	}
-};
 
-cnv.onmousedown=function(event){
-	var x = event.offsetX;
-	var y = event.offsetY;
-	ctx.beginPath();
-	ctx.moveTo(x,y);
-	painting = true;
-}
+	cnv.onmouseup=function(){
+		painting = false;
+	}
 
-
-
-cnv.onmouseup=function(){
-	painting = false;
-}
+})();
